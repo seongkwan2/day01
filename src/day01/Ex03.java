97ba39d3288b03ba2fd0783480612b619c7ea4ba
test123 <zaq3195@naver.com>
2023-04-10 14:04:33 +0900

    day01_4월10일

---
+++ src/day01/Ex03.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Ex03{
+	public static void main(String[] args) {
+		System.out.println(100);
+		System.out.println(100.123);
+		
+		System.out.println(100 + 100);
+		System.out.println(100.123 + 100.123);
+		
+		System.out.println("백 : " + 100);
+		System.out.println("백 : " + 100);
+		System.out.println("100" + 100 + 100);
+		System.out.println("100" + (100+100));
+	}
+}
